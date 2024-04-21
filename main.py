7d3cf5aafdac82f08ce68cf1914ba839e82f170e
Name <name@example.org>
2024-04-21 23:25:20 +0300

    static ip, added a new led, improved logging and readability

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import network
 import asyncio
 import time
 
-SSID = "******"
-PASS = "******"
+SSID = "123"
+PASS = "passwordXd"
 sensor = Pin(4, Pin.IN)
+activationLed = Pin(2, Pin.OUT)
+alwaysOnLed = Pin(5, Pin.OUT)
 motor = PWM(Pin(3))
 minHour = 8
 maxHour = 18
@@ -64,11 +66,25 @@ def turn(start, end, increment):
         motor.duty_u16(position)
         time.sleep(0.001)
         
+def handleTurn(pos):
+    if (pos == 0):
+        print(f"0\t-> 180\tDegrees")
+        turn(1144, 7700, 5)
+    elif (pos == 1):
+        print(f"180\t-> 0\tDegrees")
+        turn(7600, 2300, -5)
+    else:
+        print(f"Rotate to {pos}")
+        motor.duty_u16(pos)
+        
 def connectWIFI():
     net = network.WLAN(network.STA_IF)
     if not net.isconnected():
         net.active(True)
         print('Connecting...')
+        # pray to god this doesn't suddendly decide to change since the ip address
+        # is hard coded to the Android application
+        net.ifconfig(('192.168.240.169', '255.255.255.0', '192.168.240.63', '1.1.1.1'))
         net.connect(SSID, PASS)
         while not net.isconnected():
             pass
@@ -80,15 +96,17 @@ def withinHours():
     # hour = rtc.datetime()[4] # pfft don't need none of that nonsense
     # my homies prefer THIS:
     hour = int(ntptime.time() / 3600 % 24 + 3)
-    print(minHour, "<=", hour, "<=", maxHour)
-    return minHour <= hour <= maxHour
+    shouldActivate = minHour <= hour <= maxHour
+    print(f"min\tcur\tmax")
+    print(f"{minHour}\t{hour}\t{maxHour}")
+    print(f"Proceed: {shouldActivate}")
+    return shouldActivate
     
 def start():
     connectWIFI()
     ntptime.settime()
     motor.freq(50)
-    print("-> 0   Degrees")
-    motor.duty_u16(2300)
+    handleTurn(2300)
     setupServer()
     loop = asyncio.get_event_loop()
     loop.create_task(main())
@@ -102,17 +120,17 @@ def main():
     activations = 0
     while True:
         while withinHours():
-            print("Sensor:", "Not covered" if (sensor.value() == 1) else "Covered")
+            covered = "Not covered" if (sensor.value() == 1) else "Covered"
+            print(f"Sensor: {covered}")
+            alwaysOnLed.high()
+            activationLed.high()
             if sensor.value() == 0:
-                if activations % 2 == 0:
-                    print("-> 180 Degrees")
-                    turn(1144, 7700, 5)
-                else:
-                    print("-> 0   Degrees")
-                    turn(7600, 2300, -5)
+                handleTurn(activations % 2)
                 activations += 1
-            await asyncio.sleep(15)
+            activationLed.low()
+            await asyncio.sleep(10)
         else:
-            await asyncio.sleep(15)
+            alwaysOnLed.low()
+            await asyncio.sleep(60)
 
 start()
